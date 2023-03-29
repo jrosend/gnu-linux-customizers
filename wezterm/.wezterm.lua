431d9ed1fc59812d3b86416ef6cbbb97c0bcc08c
Jhonatas Rosendo <jhonatas@hotmail.com>
2023-03-29 00:23:04 -0300

    config lua script for wezterm terminal

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,10 @@
+    },
+    {
+        key = 'DownArrow',
+        mods = 'CTRL|SHIFT',
+        action = act.ActivatePaneDirection 'Down',
+    },
+  }
+
+-- and finally, return the configuration to wezterm
+return config
