ce4e0ccef03a9a8c76a70e1ee95b2bc297bc8b62
Jhonatas Rosendo <jhonatas@hotmail.com>
2023-03-29 00:32:25 -0300

    [fix]config lua script for wezterm terminal

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,3 +1,59 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+local act = wezterm.action
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+
+config.color_scheme = 'Dracula (Official)'
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = true
+config.window_decorations = 'RESIZE'
+
+config.window_close_confirmation = 'NeverPrompt'
+
+-- WSL
+
+config.default_domain = "WSL:Arch"
+config.default_prog = { "wsl.exe" } 
+
+-- KEYS
+
+config.keys = {
+    -- This will create a new vertical split and run your default program inside it
+    {
+        key = '"',
+        mods = 'CTRL|SHIFT|ALT',
+        action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+    },
+    -- This will create a new horizontal split and run your default program inside it
+    {
+        key = '%',
+        mods = 'CTRL|SHIFT|ALT',
+        action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+    },
+    {
+        key = 'LeftArrow',
+        mods = 'CTRL|SHIFT',
+        action = act.ActivatePaneDirection 'Left',
+    },
+    {
+        key = 'RightArrow',
+        mods = 'CTRL|SHIFT',
+        action = act.ActivatePaneDirection 'Right',
+    },
+    {
+        key = 'UpArrow',
+        mods = 'CTRL|SHIFT',
+        action = act.ActivatePaneDirection 'Up',
     },
     {
         key = 'DownArrow',
